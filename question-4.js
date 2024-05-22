f5eeebb5ce4a47b5698d54a71d03137e153b5a55
CharlieHii <charlie_chaper@hotmail.com>
2024-05-22 14:38:27 +0700

    feat(question-4): complete question 4

--- question-4.js
+++ question-4.js
@@ -5,3 +5,18 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+//ให้เขียนโปรแกรมในการหาสินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้า
+const findMinimumItem = function (inventory) {
+  let minItem = inventory[0];
+  for (let item of inventory) {
+    if (minItem.quantity > item.quantity) {
+      minItem = item;
+    }
+  }
+  return minItem;
+};
+
+let minItem = findMinimumItem(inventory);
+console.log(
+  `สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${minItem.name} ซึ่งมี ${minItem.quantity} ชิ้น`
+);
