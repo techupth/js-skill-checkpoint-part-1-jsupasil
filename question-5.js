93255f7be32ebecea12672552ef34e41a38d8a7e
CharlieHii <charlie_chaper@hotmail.com>
2024-05-22 14:50:15 +0700

    feat(question-5): complete question 5

--- question-5.js
+++ question-5.js
@@ -19,3 +19,21 @@ const products = [
 
 const promotionCode = "";
 // เริ่มเขียนโค้ดตรงนี้
+function calculateTotalPrice(products, promotionCode) {
+  let totalPrice = 0;
+  for (let index = 0; index < products.length; index += 1) {
+    totalPrice += products[index].quantity * products[index].price;
+  }
+  if (promotionCode === "SALE20") {
+    return (1 - 0.2) * totalPrice;
+  } else if (promotionCode === "SALE50") {
+    return (1 - 0.5) * totalPrice;
+  } else {
+    return totalPrice;
+  }
+}
+
+//TEST CASES
+console.log(calculateTotalPrice(products, ""));
+console.log(calculateTotalPrice(products, "SALE20"));
+console.log(calculateTotalPrice(products, "SALE50"));
